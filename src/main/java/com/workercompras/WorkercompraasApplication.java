c4fe7f4f6902f9f3554899fa5ff272c86282a4ae
Lucas Santos <lucas201273@gmail.com>
2024-08-05 06:37:50 -0300

    chore: get metrics to openfeign

--- src/main/java/com/workercompras/WorkercompraasApplication.java
+++ src/main/java/com/workercompras/WorkercompraasApplication.java
@@ -1,29 +1,27 @@
 package com.workercompras;
 
+import com.workercompras.config.FeignConfiguration;
 import com.workercompras.model.Endereco;
 import com.workercompras.service.CepService;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @EnableRabbit
 @EnableFeignClients
 @SpringBootApplication
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class WorkercompraasApplication {
 
-	@Autowired
-	private CepService cepService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkercompraasApplication.class, args);
 	}
 
-	@Bean
-	public 	Endereco teste(){
-		cepService.buscarCep("3121044012041240");
-		return new Endereco();
-	}
+
 }
--- src/main/java/com/workercompras/service/CepService.java
+++ src/main/java/com/workercompras/service/CepService.java
@@ -1,23 +1,27 @@
 package com.workercompras.service;
 
+import com.workercompras.config.FeignConfiguration;
 import com.workercompras.model.Endereco;
 import com.workercompras.repository.CepRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor(onConstructor_ = @__(@Autowired))
 @Slf4j
 @Service
 
 public class CepService {
 
-    private final CepRepository repository;
+    @Autowired
+    private  FeignConfiguration feignConfiguration;
+    @Autowired
+    private  CepRepository repository;
+
 
     public Endereco buscarCep(String cep){
         Endereco  endereco = repository.buscarPorCep(cep);
-        log.info(endereco.toString());
         return  endereco;
     }
 }
